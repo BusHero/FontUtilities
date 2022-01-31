2283547627ef018dff3ee3d9d8f6d773c138b568
Cervac Petru <petru.cervac@gmail.com>
2022-01-31 21:31:38 +0100

    Now I can download fonts! I am happy :D

--- Install-Font.ps1
+++ Install-Font.ps1
@@ -1,12 +1,3 @@
-# 1. Find font
-# 1.1 Download Json
-# 1.2 Parse Json
-# 2. Download font in Zip folder Done
-# 3. Unzip folder Done
-# 4. Install font Done
-# 4.1 Copy font to the C:\Windows\Fonts Done
-# 4.2 Add font name to the windows registry HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts Done
-
 [CmdletBinding()]
 param (
 	[Parameter()]
@@ -30,16 +21,19 @@ function Install-Font {
 	)
 	Copy-Item -Path $font.FullName -Destination "C:\Windows\Fonts"
 	$formatedFontName = Format-Name $font
-	New-ItemProperty -Name $formatedFontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts(Test)" -PropertyType string -Value $font.Name -Force -ErrorAction SilentlyContinue | Out-Null
+	New-ItemProperty -Name $formatedFontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $font.Name -Force -ErrorAction SilentlyContinue | Out-Null
 }
 
 function Get-Font-Family {
 	param (
 		[Parameter(Mandatory = $true)][string]$fontFamily
 	)
-	$fonts = Get-Content .\fonts.json | ConvertFrom-Json -AsHashtable
+	$fontsPath = "$($env:TEMP)\fonts-$(New-Guid).json"
 	$fontZipPath = "$($env:TEMP)\$Family_$(New-Guid)"
 	$fontZipFileName = "$fontZipPath.zip"
+
+	Invoke-WebRequest https://raw.githubusercontent.com/BusHero/Install-Font/main/fonts.json -OutFile $fontsPath 
+	$fonts = Get-Content $fontsPath | ConvertFrom-Json -AsHashtable
 	Invoke-WebRequest $fonts[$Family] -OutFile $fontZipFileName
 	Expand-Archive -LiteralPath $fontZipFileName -DestinationPath $fontZipPath
 	$fontZipPath
@@ -50,5 +44,3 @@ $fontFiles = Get-ChildItem $fontExpandPath -Filter "*.ttf"
 foreach ($font in $fontFiles) {
 	Install-Font $font
 } 
-
-Get-ItemProperty -path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts(Test)"

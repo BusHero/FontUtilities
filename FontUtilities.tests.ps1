9098c3a35defe143c8e7914daf95f5bdc4bb1128
Cervac Petru <petru.cervac@gmail.com>
2022-02-06 21:43:13 +0100

    Moved InModuleScope to BeforeAll

--- FontUtilities.tests.ps1
+++ FontUtilities.tests.ps1
@@ -1,12 +1,14 @@
 BeforeAll { 
     Import-Module .\FontUtilities.psm1
+    InModuleScope FontUtilities {
+        $script:foo = 'bar'
+    }
 }
 
 Describe "Install-FontFamily" {
-
     It "Can updated variables" {
         InModuleScope FontUtilities {
-            
+            $script:foo | should -be 'bar'
         }
     }
 }

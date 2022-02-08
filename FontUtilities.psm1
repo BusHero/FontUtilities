4c2b086bea8d6f1c7b59a4fc6a7c5956938cf6e0
Cervac Petru <petru.cervac@gmail.com>
2022-02-08 20:13:37 +0100

    Added install font

--- FontUtilities.psm1
+++ FontUtilities.psm1
@@ -1,32 +1,13 @@
-#region Variables
+$script:fonts = @{}
 
-$Script:foo = 'foo'
-$Script:ConfigUrl = 'https://raw.githubusercontent.com/BusHero/Install-Font/main/fonts.json'
-$Script:ConfigPath = "$PSScriptRoot\fonts.json"
-$Script:FontsCachePath = "$PScriptRoot\.fonts"
-
-#endregion
-
-#region Private Functions
-
-#endregion
-
-#region Public Methods
-
-function Get-Foo {
-    $script:foo
+function Add-Font {
+	param($family, $url)
+	$script:fonts[$family] = $url
 }
 
-function Update-Config {
-	Write-Host $Script:ConfigPath	
-	Invoke-WebRequest $Script:ConfigUrl -OutFile $Script:ConfigPath
-}
-
-function Write-Error($message) {
-	[Console]::ForegroundColor = 'red'
-	[Console]::Error.WriteLine($message)
-	[Console]::ResetColor()
-	exit
+function Get-Font {
+	param($family)
+	$script:fonts[$family]
 }
 
 function Format-Name {
@@ -39,55 +20,21 @@ function Format-Name {
 	}
 }
 
-function Install-Font {
-	param (
-		[Parameter(Mandatory = $true)][System.IO.FileSystemInfo]$font
-	)
-	Write-Output "Copying fonts to C:\Windows\Fonts ..."
-	Copy-Item -Path $font.FullName -Destination "C:\Windows\Fonts"
-	$formatedFontName = Format-Name $font
-
-	Write-Output "Set up register keys ..."
-	New-ItemProperty -Name $formatedFontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $font.Name -Force -ErrorAction SilentlyContinue | Out-Null
+function Install-FontFile {
+	param(
+		[string]$FontFile,
+		[string]$Location,
+		[string]$Registry)
+	Copy-Item -Path $FontFile -Destination $Location
+	
+	$font = Get-Item $FontFile
+	$formattedName = Format-Name $font
+	New-ItemProperty -Name $formattedName `
+					 -Path $Registry `
+					 -PropertyType string `
+					 -Value $font.Name `
+					 -Force `
+					 -ErrorAction SilentlyContinue | Out-Null
 }
 
-function Get-File {
-	param (
-		[string]$Source,
-		[string]$Destination
-	)
-	try {
-		Invoke-WebRequest $Source -OutFile $Destination
-	}
-	catch [Microsoft.PowerShell.Commands.HttpResponseException] {
-		Write-Output $PSItem.ErrorDetails.Message
-	}
-}
-
-function Install-FontFamily {
-	param ([Parameter()][string]$Family)
-	$fontFamilyZip = "${Script:FontsCachePath}\$Family.zip"
-	$fontFamilyPath = "${Script:FontsCachePath}\$Family"
-	$fonts = Get-Content $Script:ConfigPath | ConvertFrom-Json -AsHashtable
-	Write-Output "Downloading font '$fontFamily' font family..."
-	Get-File -Source $fonts[$Family] -Destination $fontFamilyZip
-
-	Write-Output "Unzipping archive ..."
-	Expand-Archive -LiteralPath $fontFamilyZip -DestinationPath $fontFamilyPath
-
-	$fontFiles = Get-ChildItem $fontExpandPath -Filter "*.ttf"
-
-	foreach ($font in $fontFiles) {
-		Install-Font $font
-	}
-
-	Remove-Item $fontFamilyPath -Recurse -Force
-}
-
-#endregion
-
-Export-ModuleMember Install-FontFamily
-Export-ModuleMember Update-Config
-
-Update-Config
-mkdir $Script:FontsCachePath -Force
+Export-ModuleMember -Function Add-Font, Get-Font, Install-FontFile
--- FontUtilities.tests.ps1
+++ FontUtilities.tests.ps1
@@ -1,15 +1,57 @@
+# 1. User can register fonts(Font family + url)
+# 2. User can get font's url
+
 BeforeAll { 
     Import-Module .\FontUtilities.psm1
-    InModuleScope FontUtilities {
-        $script:foo = 'bar'
-    }
 }
 
-Describe "Install-FontFamily" {
-    It "Can updated variables" {
-        InModuleScope FontUtilities {
-            $script:foo | should -be 'bar'
+# Describe "Add-Font" {
+#     Context "Get font that was added" {
+#         It "Files are copied" {
+#             $expectedUrl = "https://raw.githubusercontent.com/BusHero/test-repo/main/TestFont.zip"
+#             Add-Font -Family "TestFont" -url $expectedUrl
+#             Get-Font -Family "TestFont" | should -be $expectedUrl 
+#         }
+#     }
+#     Context "Get font that wasn't added" {
+#         It '$null is getted when no font is added' {
+#             Get-Font -Family "ThisFontDoesNotExist" | should -be $null 
+#         }
+#     }
+# }
+
+Describe "Install Font" {
+    BeforeAll {
+        $FontFileName = 'font.ttf'
+        $FontFile = "TestDrive:\$FontFileName"
+        $Location = 'TestDrive:\fonts'
+        
+        $RegistryEntry = 'font (TrueType)'
+        $RegistryName = 'Fonts'
+        $Registry = "TestRegistry:\$RegistryName"
+        
+        New-Item -Path $FontFile -ItemType File
+        New-Item -Path $Location -ItemType Directory
+        New-Item -Path 'TestRegistry:\' -Name $RegistryName
+    }
+    Context "Font is installed" {
+        BeforeAll {
+            Install-FontFile -FontFile $FontFile -Location $Location -Registry $Registry
         }
+        It "<FontFileName> is copied to the <location>" {
+            Test-Path "$Location\$FontFileName" | 
+                should -BeTrue -because "$FontFileName should be copied"
+        }
+        It "<FontFileName> is added to the <Registry>" {
+            Get-ItemProperty -path $Registry |
+                Select-object -ExpandProperty $RegistryEntry |
+                should -be $FontFileName -because 'Registry entry should be created'
+        }
+    }
+    AfterAll {
+        Remove-Item -Path $FontFile -Recurse -Force
+        Remove-Item -Path $Location -Recurse -Force
+        Remove-Item -Path $Registry -Recurse -Force
     }
 }
 
